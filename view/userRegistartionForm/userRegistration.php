fd1b85c9ee250cf80fda024b434c0d83b712d440
Somel Ahmed <somelahmed88@gmail.com>
2023-02-11 14:42:59 +0600

    userRegestrationForm Added

---
+++ view/userRegistartionForm/userRegistration.php
@@ -0,0 +1,285 @@
+<html>
+    <head></head>
+    <body>
+
+<div align = "center" >
+<h1>User Registration</h1>
+</div>
+<div align = "center">
+    <form action="">
+        <table>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+          <tr>
+            <th>First Name: </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                <input type="text" name="fname" id="" placeholder="Enter your First Name" >
+            </td>
+
+          </tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr>
+            <th>Last Name: </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                <input type="text" name="lname" id="" placeholder="Enter your Last Name" >
+            </td>
+          </tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr>
+          <th>User Name: </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                <input type="text" name="lname" id="" placeholder="Enter your User Name" >
+            </td>
+          </tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr>
+            <th>Date of Birth: </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                <input type="date" name="dateOfBirth" id="">
+            </td>
+          </tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr>
+            <th>Phone Number: </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                <input type="text" name="phoneNumberCountryCode" id="" value="+880" size="1">
+                <input type="text" name="phoneNumber" id="" placeholder="Enter your Number">
+            </td>
+          </tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr>
+            <th>Email: </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                <input type="email" name="email" id="" placeholder="abc@example.com">
+            </td>
+          </tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr>
+            <th>Gender: </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+            <input type="radio" name="gender" id="" value = "Male">Male
+            <input type="radio" name="gender" id="" value = "Female">Female
+            <input type="radio" name="gender" id="" value = "NotInterested">Not Interested
+            </td>
+          </tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr>
+            <th>City: </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+            <select id="city" name="city">
+    <option value="">Select City</option>
+    <option value="Dhaka">Dhaka</option>
+    <option value="">Gazipur</option>
+    <option value="">Mymensingh</option>
+    <option value="">Rajshahi</option>
+    <option value="">Chittagonj</option>
+    <option value="">Comilla</option>
+    <option value="">Sylhet</option>
+    <option value="">Rangpur</option>
+</select>
+            </td>
+          </tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr>
+            <th>Zip Code: </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+<input type="text" name="zipCode" id="" placeholder="Enter Zip Code">
+            </td>
+          </tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr>
+            <th>Address: </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                <textarea name="addressTextArea" id="" cols="25" rows="5"></textarea>
+            </td>
+          </tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr>
+            <th>Password: </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                <input type="password" name="password" id="" placeholder="Enter Password">
+            </td>
+          </tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr>
+            <th>Confirm Password: </th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                <input type="password" name="confirmPassword" id="" placeholder="Confirm your Password">
+            </td>
+          </tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <td>
+                <input type="checkbox" name="iAcceptCheckbox" id=""> I Accept
+            </td>
+          </tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr>
+            <td>
+                <input type="submit" value="sumbit">
+            </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            
+
+            <td>
+                <input type="reset" value="Reset">
+            </td>
+          </tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr></tr>
+          <tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <td>Already Have an Account? </td>
+            <td><a href="">login</a></td>
+          </tr>
+        </table>
+        
+    </form>
+</div>
+    </body>
+</html>
